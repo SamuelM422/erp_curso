cf73b8120f39a8cb40cbcc3584a8dc722b60cb18
Samuel Silva <samuel.m.silva0422@gmail.com>
2025-08-13 11:06:44 -0300

    Enable CORS support and update environment example file with GitHub token placeholder
    Signed-off-by: Samuel Silva <samuel.m.silva0422@gmail.com>

--- core/settings.py
+++ core/settings.py
@@ -39,9 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     ## my installed_apps
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -126,3 +128,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CORS
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
+]
