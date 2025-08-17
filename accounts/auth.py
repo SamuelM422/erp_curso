f537484f969aec0c5316cfb2492925db4d28d945
Samuel Silva <minechannelbrasil@gmail.com>
2025-08-17 20:25:09 -0300

    Add `Employee` model and `Authentication` class with signup/signin functionality

---
+++ accounts/auth.py
@@ -0,0 +1,60 @@
+from rest_framework.exceptions import AuthenticationFailed, APIException
+from django.contrib.auth.hashers import check_password, make_password
+from django.contrib.auth import get_user_model
+from companies.models import Enterprise, Employee
+
+User = get_user_model()
+
+class Authentication:
+    def signin(self, email=None, password=None):
+        exception_auth = AuthenticationFailed('Email or password incorrect')
+        user_exists = User.objects.filter(email=email).exists()
+
+        if not user_exists:
+            raise exception_auth
+
+        user = User.objects.get(email=email)
+
+        if not check_password(password, user.password):
+            raise exception_auth
+
+        return user
+
+    def signup(self, name, email, password, type_account='owner', company_id=False):
+        if not name or name == '':
+            raise APIException('Name is required')
+
+        if not email or email == '':
+            raise APIException('Email is required')
+
+        if not password or password == '':
+            raise APIException('Password is required')
+
+        if type_account == 'employee' and not company_id:
+            raise APIException('Type account is required')
+
+        if User.objects.filter(email=email).exists():
+            raise APIException('Email already exists')
+
+        password_hashed = make_password(password)
+
+        created_user = User.objects.create(
+            name=name,
+            email=email,
+            password=password_hashed,
+            is_owner=type_account == 'owner'
+        )
+
+        if type_account == 'owner':
+            Enterprise.objects.create(
+                name='Enterprise name',
+                user_id=created_user
+            )
+
+        if type_account == 'employee':
+            Employee.objects.create(
+                enterprise_id=company_id,
+                user_id=created_user.pk
+            )
+
+        return created_user
